53dd6c5f7b7f95c769da9f9be66445589a4f1031
ABANI SINGHA <ma23m001@smail.iitm.ac.in>
2024-10-21 00:07:22 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+INPUT_DIR = "../input/sample_invoices/"  # Path to the folder containing PDFs
+OUTPUT_CSV = "../output/invoice_report.csv"  # Path to the output CSV
+
+# API key for Gemini OCR
+GEMINI_API_KEY = "your_api_key_here"
+GEMINI_API_ENDPOINT = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateCo"
